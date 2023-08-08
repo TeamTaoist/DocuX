8e2e4764a3112bca25e7f64e45bcf825376edb8b
sr <7518647+wendychaung@users.noreply.github.com>
2023-08-08 14:59:36 +0800

    update history

--- public/js/cover.js
+++ public/js/cover.js
@@ -44,7 +44,8 @@ const options = {
 <!--              <div class="ui-history-pin fa fa-thumb-tack fa-fw"></div>-->
               <div class="ui-history-close fa fa-close fa-fw" data-toggle="modal" data-target=".delete-history-modal"></div>
 <!--              <div class="content">-->
-                   <img src="<%- serverURL %>/fengmian.png" alt="" class="TopBg">
+                    <div class="TopBg"><img src="/fengmian.png" alt=""></div>
+
                 <h4 class="text"></h4>
 <!--                <p>-->
 <!--                  <i><i class="fa fa-clock-o"></i> visited </i><i class="fromNow"></i>-->
